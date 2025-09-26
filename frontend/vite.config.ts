2a1b8e0c77aa0e3a983024b6e9c3cc2f42b650f8
Bastian <bastian.proboste@proton.me>
2025-09-26 17:18:08 -0300

    Resubida

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -3,5 +3,10 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
+  server: {
+    host: true, // permite exponer en red local
+    port: 5173, // opcional, puedes cambiarlo si quieres otro puerto
+    allowedHosts: ['ua.bastianproboste.work'],
+  },
   plugins: [react()],
 })
