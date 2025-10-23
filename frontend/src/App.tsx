27eda309d5db2912b6b8c3bf169b07ba27904493
Bastian <bastian.proboste108@gmail.com>
2025-10-23 00:04:55 -0300

    redirect To Login

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,7 +1,9 @@
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
 import { ThemeProvider } from '@mui/material/styles';
 import { CssBaseline } from '@mui/material';
 import { theme } from './theme/theme'; // Import nombrado, no default
+
+
 import Login from './pages/login';
 import RegisterEstudiantes from './pages/registerEstudiantes';
 import DashboardEstudiante from './pages/dashboardEstudiante';
@@ -32,49 +34,48 @@ function App() {
   return (
     <ThemeProvider theme={theme}>
       <CssBaseline />
-        <Router>
-          <Routes>
-            <Route path="/login" element={<Login />} />
-            <Route path="/register" element={<RegisterEstudiantes />} />
-            <Route
-              path="/register-estudiantes"
-              element={<RegisterEstudiantes />}
-            />
-            <Route path="/auth/callback" element={<AuthCallback />} />
-            <Route path="/evaluacion-supervisor/:token" element={<EvaluacionSupervisorPublica />} />
-            <Route path="/forgot-password" element={<ForgotPassword />} />
-            <Route path="/reset-password" element={<ResetPassword />} />
-            <Route path="/autoevaluacion" element={<Autoevaluacion />} />
-            <Route path="/historial_solicitudes" element={<HistorialSolicitudes />} />
+      <Router>
+        <Routes>
+          <Route path="/" element={<Navigate to="/login" replace />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/register" element={<RegisterEstudiantes />} />
+          <Route path="/register-estudiantes" element={<RegisterEstudiantes />} />
+          <Route path="/auth/callback" element={<AuthCallback />} />
+          <Route path="/evaluacion-supervisor/:token" element={<EvaluacionSupervisorPublica />} />
+          <Route path="/forgot-password" element={<ForgotPassword />} />
+          <Route path="/reset-password" element={<ResetPassword />} />
+          <Route path="/autoevaluacion" element={<Autoevaluacion />} />
+          <Route path="/historial_solicitudes" element={<HistorialSolicitudes />} />
 
-            {/* Rutas protegidas por autenticación */}
-            <Route path='/' element={<RouteGuard />}>
+          {/* Rutas protegidas por autenticación */}
+          <Route element={<RouteGuard />}>
 
-              {/* Rutas para el rol de estudiante */}
-              <Route path='/estudiante' element={<RouteGuard roleAllowed='estudiante' />}>
-                <Route path="/estudiante/dashboard" element={<DashboardEstudiante />} />
-                <Route path='/estudiante/autoevaluacion' element={<SeleccionPractica />} />
-                <Route path='/estudiante/seleccion-practica' element={<SeleccionPractica />} />
-                <Route path='/estudiante/autoevaluacion/:practicaId' element={<Autoevaluacion />} />
-                <Route path='/estudiante/fichapractica' element={<PracticaProfesionalForm />} />
-                <Route path="/estudiante/adjuntar_informes" element={<AdjuntarInformes />} />
-                <Route path="/estudiante/retroalimentacion" element={<Retroalimentacion />} />
-                <Route path="/estudiante/empresas" element={<EstudianteEmpresas />} />
-                <Route path="/estudiante/preguntas-frecuentes" element={<PreguntasFrecuentes />} />
-              </Route>
-
-              {/* Rutas para el rol de coordinador */}
-              <Route path='/coordinador' element={<RouteGuard roleAllowed='coordinador' />}>
-                <Route path="/coordinador/dashboard" element={<DashboardCoordinador />} />
-                <Route path='/coordinador/practicas' element={<CoordinadorPracticas />} />
-                <Route path='/coordinador/estudiantes' element={<CoordinadorEstudiantes />} />
-                <Route path='/coordinador/empresas' element={<CoordinadorEmpresas />} />
-                <Route path='/coordinador/informes-estudiante' element={<CoordinadorInformesEstudiantes />} />
-                <Route path="/coordinador/evaluar-informe/:informeId" element={<CoordinadorEvaluarInforme />} />
-                <Route path='/coordinador/autoevaluaciones' element={<ListaAutoevaluaciones />} />
-                <Route path='/coordinador/calificar-autoevaluacion/:autoevaluacionId' element={<CalificarAutoevaluacion />} />
-              </Route>
+            {/* Rutas para el rol de estudiante */}
+            <Route path="/estudiante" element={<RouteGuard roleAllowed='estudiante' />}>
+              <Route path="dashboard" element={<DashboardEstudiante />} />
+              <Route path="autoevaluacion" element={<SeleccionPractica />} />
+              <Route path="seleccion-practica" element={<SeleccionPractica />} />
+              <Route path="autoevaluacion/:practicaId" element={<Autoevaluacion />} />
+              <Route path="fichapractica" element={<PracticaProfesionalForm />} />
+              <Route path="adjuntar_informes" element={<AdjuntarInformes />} />
+              <Route path="retroalimentacion" element={<Retroalimentacion />} />
+              <Route path="empresas" element={<EstudianteEmpresas />} />
+              <Route path="preguntas-frecuentes" element={<PreguntasFrecuentes />} />
             </Route>
+
+            {/* Rutas para el rol de coordinador */}
+            <Route path="/coordinador" element={<RouteGuard roleAllowed='coordinador' />}>
+              <Route path="dashboard" element={<DashboardCoordinador />} />
+              <Route path="practicas" element={<CoordinadorPracticas />} />
+              <Route path="estudiantes" element={<CoordinadorEstudiantes />} />
+              <Route path="empresas" element={<CoordinadorEmpresas />} />
+              <Route path="informes-estudiante" element={<CoordinadorInformesEstudiantes />} />
+              <Route path="evaluar-informe/:informeId" element={<CoordinadorEvaluarInforme />} />
+              <Route path="autoevaluaciones" element={<ListaAutoevaluaciones />} />
+              <Route path="calificar-autoevaluacion/:autoevaluacionId" element={<CalificarAutoevaluacion />} />
+            </Route>
+          </Route>
+          <Route path="*" element={<Navigate to="/login" replace />} />
           </Routes>
         </Router>
     </ThemeProvider>
